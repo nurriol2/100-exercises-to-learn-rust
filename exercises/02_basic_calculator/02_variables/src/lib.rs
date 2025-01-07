2d01e1654ea44f042cef34a520f227cc726cf9eb
nurriol2 <niko.urriola1@gmail.com>
2025-01-07 14:36:39 -0600

    Revert "Define variable using function args."
    This reverts commit 945ec89c2a6f71f31aa564d8d28c33fe6e721911.

--- exercises/02_basic_calculator/02_variables/src/lib.rs
+++ exercises/02_basic_calculator/02_variables/src/lib.rs
@@ -9,9 +9,6 @@ pub fn speed(start: u32, end: u32, time_elapsed: u32) -> u32 {
     // TODO: define a variable named `distance` with the right value to get tests to pass
     //  Do you need to annotate the type of `distance`? Why or why not?
 
-    // No because the compiler can infer the type.
-    let distance = end - start;
-
     // Don't change the line below
     distance / time_elapsed
 }
