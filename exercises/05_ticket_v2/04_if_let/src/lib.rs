be5f0fc4bfada59a1f730128d0329d09e107f88e
nurriol2 <niko.urriola1@gmail.com>
2025-02-05 17:05:39 -0600

    Add accessor method for a circle radius
    This solution shows examples of using `if let` and `let/else` to solve the problem.
    The main takeaway from this exercise is that both Rust idioms destructure one
    struct-like variant into the data of its fields.

--- exercises/05_ticket_v2/04_if_let/src/lib.rs
+++ exercises/05_ticket_v2/04_if_let/src/lib.rs
@@ -8,7 +8,14 @@ impl Shape {
     // TODO: Implement the `radius` method using
     //  either an `if let` or a `let/else`.
     pub fn radius(&self) -> f64 {
-        todo!()
+        // if let Shape::Circle { radius } = self {
+        //     *radius
+        // } else {
+        //     panic!("Shape must be a `Circle` to call this method.");
+        // }
+
+        let Shape::Circle {radius} = self else {panic!("Shape must be a circle to call this method.")};
+        *radius
     }
 }
 
