a479235525f404da670122022f3d4e96f984fe40
nurriol2 <niko.urriola1@gmail.com>
2025-02-04 19:47:09 -0600

    Implement common traits for `SaturatingU16` type
    This exercise can be broken down into sub-problems based on the
    trait being implemented.
    At almost every step, this solution takes advantage of symmetry
    within the sub-problem to easily implement the trait for certain
    cases (i.e. borrowed cases).
    
    Since `SaturatingU16` already holds a `u16` value, converting
    `From<u16>` is a matter of binding the data to the `value` field.
    Converting `From<u8>` is the same problem, but we need to turn
    `u8` into `u16`, first, with `into()`.
    Solving the borrowed (`&`) versions of these sub-problems adds
    the extra step of dereferencing (`*`), before relying on the
    original `From` implementations.
    
    Implementing the `Add` trait is done similarly.
    In this case, `Add<SaturatingU16> for SaturatingU16` and `Add<u16>
    for SaturatingU16` are the base cases.
    To satisfy the condition that addition saturates at the value for
    `u16`, these base cases use the `saturating_add()` function.
    
    The borrowed versions dereference the values first, then use `+` to
    add values together.
    It's possible to just use `+` here because `+` is syntactic sugar
    for `Add::add()`, which was implemented for the dereferenced types,
    above.
    
    Finally, this exercise requires that we use different methods to
    implement the `PartialEq` trait.
    Deriving `PartialEq` unlocks comparison with other `SaturatingU16`.
    Writing a custom implementation of `PartialEq::eq` also unlocks
    comparison with different types.
    This solution implements `PartialEq<u16>` based on the example in the standard library documentation.

--- exercises/04_traits/14_outro/src/lib.rs
+++ exercises/04_traits/14_outro/src/lib.rs
@@ -1,10 +1,96 @@
 // TODO: Define a new `SaturatingU16` type.
-//   It should hold a `u16` value.
-//   It should provide conversions from `u16`, `u8`, `&u16` and `&u8`.
-//   It should support addition with a right-hand side of type
-//   SaturatingU16, u16, &u16, and &SaturatingU16. Addition should saturate at the
-//   maximum value for `u16`.
-//   It should be possible to compare it with another `SaturatingU16` or a `u16`.
-//   It should be possible to print its debug representation.
+//   SaturatingU16 should hold a `u16` value.
+//
+//   SaturatingU16 should provide conversions from `u16`
+//   SaturatingU16 should provide conversions from `u8`
+//
+//   SaturatingU16 should provide conversions from `&u16`
+//   SaturatingU16 should provide conversions from `&u8`.
+//
+//   Addition should saturate at the maximum value for `u16`.
+//
+//   SaturatingU16 should support addition, right-hand side of type SaturatingU16
+//   SaturatingU16 should support addition, right-hand side of type u16
+//
+//   SaturatingU16 should support addition, right-hand side of type &u16
+//   SaturatingU16 should support addition, right-hand side of type &SaturatingU16
+//
+//   It should be possible to compare it with `u16`.
+//   It should be possible to compare it with another `SaturatingU16`
+//
+//   It should be possible to print SaturatingU16  debug representation.
 //
 // Tests are located in the `tests` folder—pay attention to the visibility of your types and methods.
+
+use std::ops::Add;
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct SaturatingU16 {
+    value: u16,
+}
+
+impl From<u16> for SaturatingU16 {
+    fn from(value: u16) -> Self {
+        Self { value }
+    }
+}
+
+impl From<u8> for SaturatingU16 {
+    fn from(value: u8) -> Self {
+        Self {
+            value: value.into(),
+        }
+    }
+}
+
+impl From<&u16> for SaturatingU16 {
+    fn from(value: &u16) -> Self {
+        (*value).into()
+    }
+}
+
+impl From<&u8> for SaturatingU16 {
+    fn from(value: &u8) -> Self {
+        (*value).into()
+    }
+}
+
+impl Add<SaturatingU16> for SaturatingU16 {
+    type Output = SaturatingU16;
+
+    fn add(self, rhs: SaturatingU16) -> Self::Output {
+        (self.value.saturating_add(rhs.value)).into()
+    }
+}
+
+impl Add<u16> for SaturatingU16 {
+    type Output = SaturatingU16;
+
+    fn add(self, rhs: u16) -> Self::Output {
+        (self.value.saturating_add(rhs)).into()
+    }
+}
+
+impl Add<&u16> for SaturatingU16 {
+    type Output = SaturatingU16;
+
+    fn add(self, rhs: &u16) -> Self::Output {
+        // `+` is the same as `Add<u16> for SaturatingU16`, above
+        (self + *rhs).into()
+    }
+}
+
+impl Add<&SaturatingU16> for SaturatingU16 {
+    type Output = SaturatingU16;
+
+    fn add(self, rhs: &SaturatingU16) -> Self::Output {
+        // `+` is the same as `Add<SaturatingU16> for SaturatingU16`, above
+        self + *rhs
+    }
+}
+
+impl PartialEq<u16> for SaturatingU16 {
+    fn eq(&self, other: &u16) -> bool {
+        self.value == *other
+    }
+}
